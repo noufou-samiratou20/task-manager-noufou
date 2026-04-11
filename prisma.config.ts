b9657e8ebb873e85f14c0ace1c06a0df1222439c
noufou-samiratou20 <noufou.samiratou20@gmail.com>
2026-04-11 18:44:16 +0000

    test: trigger CI/CD pipeline SAMI

--- prisma.config.ts
+++ prisma.config.ts
@@ -8,6 +8,6 @@ export default defineConfig({
     seed: "tsx prisma/seed.ts",
   },
   datasource: {
-    url: process.env["DATABASE_URL"] || "file:./tasks.db",
+    url: "file:./tasks.db",
   },
 });
